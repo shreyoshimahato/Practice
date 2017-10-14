fbbe741a71cc0f486096b8ce9cd93a2270c97a65
shreyoshimahato <shreyoshi.mahato@brightlifecare.com>
2017-10-14 12:50:14 +0530

    bad

--- src/sku/cg/TryV.java
+++ src/sku/cg/TryV.java
@@ -10,9 +10,9 @@ public class TryV {
 		String str="sopholua";
 		System.out.println(str);
 		System.out.println("hello");
-		int a = 9;
+		int a = 10;
 		int b = 10;
-		System.out.println(a + b);
+		System.out.println(a * b);
 		String str1 = "tere";
 		System.out.println(str1);
 	}
