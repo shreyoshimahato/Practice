91fc99713894e1e6a7efd992861f47a9e15d396d
shreyoshimahato <shreyoshi.mahato@brightlifecare.com>
2017-10-14 10:44:53 +0530

    6

--- src/sku/cg/TryV.java
+++ src/sku/cg/TryV.java
@@ -3,6 +3,8 @@ package sku.cg;
 public class TryV {
 
 	public static void main(String[] args) {
+		String str="tata";
+		System.out.println(str);
 		System.out.println("hello");
 		int a=9;
 		int b=10;
