1715cd60d591d3072c7c353320fb25bb1a2465e8
shreyoshimahato <32060875+shreyoshimahato@users.noreply.github.com>
2017-10-14 12:44:23 +0530

    Update TryV.java

--- src/sku/cg/TryV.java
+++ src/sku/cg/TryV.java
@@ -6,16 +6,10 @@ public class TryV {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		String str="sopholua";
-		System.out.println(str);
-		System.out.println("hello");
-
-		int a = 9;
+		int a = 10;
 		int b = 10;
-		System.out.println(a + b);
-		String str1 = "tere";
-		System.out.println(str1);
+		System.out.println(a * b);
+		
 	}
 
 }
